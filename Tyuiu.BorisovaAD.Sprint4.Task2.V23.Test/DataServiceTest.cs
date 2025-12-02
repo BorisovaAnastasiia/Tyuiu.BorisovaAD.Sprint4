29f6e94d7d78ee60089d599e0319d160236d9fb3
Anastasiia Borisova <kuqhoxp@gmail.com>
2025-12-02 19:05:53 +0500

    Task2

---
+++ Tyuiu.BorisovaAD.Sprint4.Task2.V23.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.BorisovaAD.Sprint4.Task2.V23.Lib;
+namespace Tyuiu.BorisovaAD.Sprint4.Task2.V23.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            // Тестовый массив (сумма четных: 4+6+8+4+6 = 28)
+            int[] array = { 3, 4, 5, 6, 7, 8, 3, 4, 5, 6, 7, 8, 3, 4, 5 };
+            int expected = 28;
+            int actual = ds.Calculate(array);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Tyuiu.BorisovaAD.Sprint4.Task2.V23/Program.cs
@@ -0,0 +1,50 @@
+﻿using Tyuiu.BorisovaAD.Sprint4.Task2.V23.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Инициализация генератора случайных чисел
+        Random rnd = new Random();
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #4 | Выполнил: Борисова А.Д. | Смартб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #4                                                               *");
+        Console.WriteLine("* Тема: Базовые навыки работы c массивами                                 *");
+        Console.WriteLine("* Задание #2                                                              *"); // Исправлено на Задание #2
+        Console.WriteLine("* Вариант #23                                                             *"); // Исправлено на Вариант #23
+        Console.WriteLine("* Выполнил: Борисова Анастасия Дмитриевна | Смартб-25-1                   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Дан одномерный целочисленный массив на 15 элементов заполненный " +
+            "случайными значениями в диапазоне от 3 до 8 подсчитать сумму четных элементов массива."); // Исправлено условие
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+        Console.WriteLine("***************************************************************************");
+
+        const int len = 15; // Длина массива 15 элементов
+        int[] array = new int[len];
+
+        // Заполнение массива случайными числами от 3 до 8 (верхняя граница Exclusive, поэтому 9)
+        for (int i = 0; i < len; i++)
+        {
+            array[i] = rnd.Next(3, 9);
+        }
+
+        Console.WriteLine("Массив:");
+        for (int i = 0; i < len; i++)
+        {
+            Console.Write(array[i] + " ");
+        }
+        Console.WriteLine();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        // Вызываем метод Calculate из библиотеки V23
+        int sum = ds.Calculate(array);
+        Console.WriteLine($"Сумма четных элементов массива: {sum}"); // Улучшенный вывод результата
+        Console.ReadKey();
+    }
+}
